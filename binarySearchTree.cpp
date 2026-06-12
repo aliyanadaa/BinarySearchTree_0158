2b66615dbdffb848a5d020de6e6e9095829c5d7d
aliyanadaa <aliyanadaq@gmail.com>
2026-06-12 15:48:29 +0700

    menambahkan fungsi search

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -44,4 +44,21 @@ public:
             parent->rightChild = newNode;
         }
     }
+
+    void search(string element, Node *&parent, Node *&currentNode)
+    {
+        currentNode = ROOT;
+        parent = NULL;
+
+        while ((currentNode != NULL) &&
+            (currentNode->info != element))
+        {
+            parent = currentNode;
+
+            if (element < currentNode->info)
+                currentNode = currentNode->leftChild;
+            else
+            c   urrentNode = currentNode->rightChild;
+        }
+    }
 };
